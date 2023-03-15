1977882044284e1c182297e02b480e4e633dfb86
binamralamsal <binamralamsal@gmail.com>
2023-03-15 15:04:05 +0545

    Finalized

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,7 +41,6 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
